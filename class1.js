64357068c8d396fde1500b15f852333320b6986b
Tanu Mangal <tanu.mangal_cs24@gla.ac.in>
2025-11-21 08:45:15 +0530

    Add files via upload

---
+++ class1.js
@@ -0,0 +1,48 @@
+// Function to calculate age and print years
+// function calculateAge(birthYear) {
+//     const currentYear = 2025; 
+//     let age = 0;
+    
+//     console.log("Years from the current year:");
+    
+
+//     for(let year = birthYear; year <= currentYear; year++) {
+//         age = year - birthYear;
+//         console.log(`Year ${year}: Age ${age}`);
+//     }
+    
+//     console.log("\nCurrent age:", age);
+//     return age;
+// }
+// calculateAge(2000);
+
+
+
+
+// function calculation(calculation,num1,num2){
+//     console.log(calculation(num1,num2));
+// }
+
+// const sum = function(num1,num2){
+//     return num1 + num2;
+// }
+// const product = function(num1,num2){
+//     return num1 * num2;
+// }               
+
+// calculation(sum,5,10);
+// calculation(product,5,10);
+
+
+//const arr = [3,4,5,6,7,58,26,78,3,87,5];
+//  const answer= arr.find(function(num){
+//     return  num % 29 === 0 ;
+//  })
+
+// const divisible = function(num){
+//       return num % 2 !== 0 ;
+//    };
+//    const answer = aar.filter(divisible );
+// console.log(answer);
+
+
